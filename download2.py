b8ca3d6381e754db99c70a2132e85c3867af3b91
Ranuga-Disansa <79456372+Programmer-RD-AI@users.noreply.github.com>
2021-10-08 16:49:46 +0530

    Add files via upload

--- download2.py
+++ download2.py
@@ -1,9 +1,10 @@
-idx = 0
 import pandas as pd
 from tqdm import tqdm
+
+idx = 0
+
 labels = [
     "Business card",
-    
     "Collectible card game",
     "Debit card",
     "Penalty card",
@@ -11,21 +12,67 @@ labels = [
     "Telephone card",
 ]
 labels_r = ["/m/01sdgj", "/m/0216z", "/m/02h5d", "/m/02wvcj0", "/m/03r8dh", "/m/066zr"]
-annotations = pd.read_csv('./V2/data/oidv6-train-annotations-human-imagelabels.csv')
+annotations = pd.read_csv("./V2/data/oidv6-train-annotations-human-imagelabels.csv")[:25000]
+print(annotations.columns)
 bs = []
-for a,b in tqdm(zip(annotations['LabelName'],annotations['ImageID'])):
+for a, b in tqdm(zip(annotations["LabelName"], annotations["ImageID"])):
     if a in labels_r:
         idx += 1
     bs.append(b)
 print(idx)
-annotations = pd.read_csv('./V2/data/oidv6-train-annotations-bbox.csv')
+annotations = pd.read_csv("./V2/data/oidv6-train-annotations-bbox.csv")[:25000]
+print(annotations.columns)
 idx = 0
 idx_other = 0
-for imgid,labelname in tqdm(zip(annotations['ImageID'],annotations['LabelName'])):
-    if imgid in bs:
+imgids = []
+imageids = []
+for imgid in tqdm(
+    zip(
+        annotations["ImageID"],
+        annotations["XMin"],
+        annotations["YMin"],
+        annotations["XMax"],
+        annotations["YMax"],
+    )
+):
+    # print(imgid)
+    if imgid[0] in bs:
         idx += 1
-    if labelname in labels_r or labelname in labels:
-        idx_other += 1
-    labels_r
+        imgids.append(imgid)
+        imageids.append(imgid[0])
 print(idx)
-print(idx_other)
+annotations = pd.read_csv("./V2/data/oidv6-train-images-with-labels-with-rotation.csv")[:25000]
+print(annotations.columns)
+data = {
+    "ImageID": [],
+    "OriginalURL": [],
+    "OriginalLandingURL": [],
+    "XMin": [],
+    "YMin": [],
+    "XMax": [],
+    "YMax": [],
+}
+for imgid in tqdm(
+    zip(
+        annotations["ImageID"],
+        annotations["OriginalURL"],
+        annotations["OriginalLandingURL"],
+    )
+):
+    print(imgid)
+    print(imageids)
+    break
+    if imgid[0] in imageids:
+        print(imgids)
+        data["ImageID"] = imgid[0]
+        data["OriginalURL"] = imgid[1]
+        data["OriginalLandingURL"] = imgid[2]
+        data["XMin"].append(imgid[1])
+        data["YMin"].append(imgid[2])
+        data["XMax"].append(imgid[3])
+        data["YMax"].append(imgid[4])
+# print(imageids)
+# print(imgids)
+# print(data)
+data = pd.DataFrame(data)
+data.to_csv("./V2/data/Cleaned-Data.csv", index=False)
